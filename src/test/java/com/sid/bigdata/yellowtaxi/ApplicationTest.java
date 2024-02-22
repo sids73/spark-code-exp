509a12d900d05a04fb3aef64d3bdcd5d08236b3d
Siddhesh C Sankulkar <sids73@yahoo.com>
2024-02-21 23:11:16 -0500

    Refactored test code based on Intellij suggestions

--- src/test/java/com/sid/bigdata/yellowtaxi/ApplicationTest.java
+++ src/test/java/com/sid/bigdata/yellowtaxi/ApplicationTest.java
@@ -1,19 +1,15 @@
 package com.sid.bigdata.yellowtaxi;
 
-
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-
 import java.io.File;
 import java.io.IOException;
-
 import org.junit.Test;
 
 public class ApplicationTest {
 @Test
 public void yellowTaxiDataCruncherTest() throws IOException {
-		String argString[]={"develop", 
+		String[] argString ={"develop",
 							"E:/Hadoop_Home/bin/hadoop.dll",
 							"src/test/resources/sample-data/yellow_taxi/input_data/sample-data.csv",
 		            		"src/test/resources/sample-data/yellow_taxi/output_data/"};
@@ -27,31 +23,18 @@ public void yellowTaxiDataCruncherTest() throws IOException {
 
 private static Double sumAverageTaxiVendorAmountsShare(File outputDir) throws IOException {
 	File[] files = outputDir.listFiles();
-	Double vendorAmtSum=0.0;
+	double vendorAmtSum=0.0;
+    assert files != null;
     for (File file : files) {
         if (file.isDirectory()) {
            String dirName=file.getCanonicalPath();
            if (dirName.contains("compared_to_average_total_amount_by_vendor=")){
-        	   String vendorArray[] = dirName.split("=");
-        	   vendorAmtSum+=Double.valueOf(vendorArray[1]);
+        	   String[] vendorArray = dirName.split("=");
+        	   vendorAmtSum+=Double.parseDouble(vendorArray[1]);
            }
         
         }
     }
 	return vendorAmtSum;
 }
-
-//private static void displayDirectoryContents(File dir) throws IOException {
-// 
-//        File[] files = dir.listFiles();
-//        for (File file : files) {
-//            if (file.isDirectory()) {
-//                System.out.println("Directory Name==>:" + file.getCanonicalPath());
-//               // displayDirectoryContents(file);
-//            } else {
-//                System.out.println("file Not Acess===>" + file.getCanonicalPath());
-//            }
-//        }
-//  
-//}
 }
